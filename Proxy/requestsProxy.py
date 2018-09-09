f45157af97a248636e76365d03395c67459aa483
anchor <netuser.orz@icloud.com>
2018-09-09 16:54:51 +0800

    BOH 测试工具类

--- Proxy/requestsProxy.py
+++ Proxy/requestsProxy.py
@@ -3,13 +3,13 @@
 import requests
 
 proxies = {
-    'https': 'https://127.0.0.1:56948',
-    'http': 'http://127.0.0.1:56948'
+    'https': 'https://127.0.0.1:49621',
+    'http': 'http://127.0.0.1:49620'
 }
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
 }
 
-google_url = 'https://www.youtube.com'
-response = requests.get(google_url, proxies=proxies)
+google_url = 'http://123.232.10.234'
+response = requests.get(google_url, proxies=proxies,headers=headers)
 print(response.text)
