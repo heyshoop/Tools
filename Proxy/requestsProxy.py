65d305ce5de86673f3eac39897710f10bf2b5990
anchor <netuser.orz@icloud.com>
2018-10-09 17:11:39 +0800

    测试工具类

--- Proxy/requestsProxy.py
+++ Proxy/requestsProxy.py
@@ -10,6 +10,6 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
 }
 
-google_url = 'http://123.232.10.234'
+google_url = 'https://www.youtube.com'
 response = requests.get(google_url, proxies=proxies,headers=headers)
 print(response.text)
--- Proxy/urllibProxy.py
+++ Proxy/urllibProxy.py
@@ -11,7 +11,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
 }
 
-google_url = 'http://123.232.10.234:8083/servlet/com.sdjxd.pms.platform.serviceBreak.Invoke'
+google_url = 'https://www.youtube.com'
 opener = request.build_opener(request.ProxyHandler(proxies))
 request.install_opener(opener)
 
